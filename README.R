267d2839ab19e8db7efef0fbe55bb3b545b3f267
Tobie Surette <tobie.surette@gmail.com>
2020-04-21 10:06:22 -0300

    Update readme file

--- README.R
+++ README.R
@@ -10,10 +10,10 @@ writeLines(readLines("metadata/abstract.md"), con = readme.file, sep = "\n")
 writeLines("\n## Methods\n", con = readme.file, sep = "\n")
 writeLines(readLines("metadata/methods.md"), con = readme.file, sep = "\n")
 
-close(readme.file)
-
 ## Authors:
 cat("\n## Authors\n", file = file, append = TRUE)
 cat("* **Tobie Surette**\n", file = file, append = TRUE)
 cat("* **Denis Gagnon**\n", file = file, append = TRUE)
 cat("* **Natalie Asselin**\n", file = file, append = TRUE)
+
+close(readme.file)
